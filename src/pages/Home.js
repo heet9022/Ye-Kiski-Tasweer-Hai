d434e077fb22ab62b152126d5b608c211345de3d
Harshit <harshitshah4@gmail.com>
2020-03-08 02:38:35 +0530

    Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,27 @@ import axios from 'axios';
 
 import Upload from '../components/Upload'
 
+
+function PostCard(props){
+
+  return (
+    <div>
+      
+      <img style={{width:"100%",height:"100%",maxHeight:"25vw",maxHeight:"20vh"}} src={props.post.image}/>
+      {
+        props.post.label && <h4>Label : {props.post.label}</h4>
+      }
+      {
+        props.post.confidence && <h4>Confidence : {props.post.confidence}</h4>
+      }
+      
+    </div>
+  )
+
+}
+
+
+
 class Home extends React.Component{
 
     constructor(props)
@@ -19,8 +40,10 @@ class Home extends React.Component{
 
       this.state = {
         files: [],
+        posts:[],
         submit:false,
-        imagePreview: false,
+        imagePreview:true,
+        selected:0
 
       }
 
@@ -28,25 +51,16 @@ class Home extends React.Component{
       this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleDrop = (files) => {
+    handleDrop = (e) => {
+      const files = e.target.files;
+
       const newState = update(this.state,{
         files:{
           $push : Array.from(files),
           
         }
       })
-      this.setState(newState,()=>{
-        console.log(this.state);
-      })
-
-     if (this.state.files.length > 0)
-     {
-      this.setState({imagePreview:true});
-     } 
-     else 
-     {
-      this.setState({imagePreview:false});
-     }
+      this.setState(newState)
     }
 
     handleSubmit(e) {
@@ -69,8 +83,14 @@ class Home extends React.Component{
                         posts.push(Object.assign({},{image:signedResponse.data.id}));  
                     }
                 }
-                await customAxios.post('post',{posts:posts});
-      
+                const pids = (await customAxios.post('post',{posts:posts})).data || [];
+                console.log(pids)
+
+                const processedPosts = (await customAxios.post('model/test',{pids:pids})).data || []
+
+                this.setState({posts:processedPosts,submit:true});
+
+
             }catch(e){
                 console.log(e);
                 console.log("Something Went Wrong");
@@ -78,41 +98,72 @@ class Home extends React.Component{
         }
     )();
     
-    this.setState({submit:true});
+    
     }
 
     render(){
-      var processedFiles = []
+      
 
-      for(var i=0;i<this.state.files.length;i++){
-        processedFiles.push(Object.assign({},{image:URL.createObjectURL(this.state.files[i]),label:null,confidence:null}))
-      }
+      // var upload = null;
 
-      let upload = (this.state.imagePreview)?<ViewGrid files={processedFiles} submit={this.state.submit}></ViewGrid>:<div className="Status">Drag Files here</div>; 
+      // if(!this.state.submit){
+      //   if(this.state.imagePreview){
+      //     var processedFiles = []
+
+      //     for(var i=0;i<this.state.files.length;i++){
+      //       processedFiles.push(Object.assign({},{image:URL.createObjectURL(this.state.files[i]),label:null,confidence:null}))
+      //     }
+
+      //     upload = <ViewGrid posts={processedFiles} submit={this.state.submit}></ViewGrid>
+      //   }else{
+      //     upload = <div>Drag Files Here</div>
+      //   }
+      // }else{
+      //   if(this.state.posts.length>0){
+      //     upload = <ViewGrid posts={this.state.posts} submit={this.state.submit}></ViewGrid>  
+      //   }else{
+      //     upload = "h1"
+      //   }
+      // }
+
+      // console.log(upload)
 
       return(
-        <div>
-          
-          <DragAndDrop handleDrop={this.handleDrop} >
+        <div style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
+          <div>
+            {
+              !this.state.submit && 
+                <div> 
+                  <input type="file" accept="image/*" multiple={true} onChange={this.handleDrop}/>
+                </div>
+            }
             
-            {/* <div style={{display:"grid",gridTemplateColumns:"1fr 1fr 1fr 1fr"}}>
-              
+            <div style={{display:"grid",gridTemplateColumns:"1fr 1fr 1fr 1fr",gridGap:"50px",maxHeight:"60vh",overflowY:"scroll"}}>
               {
-                this.state.files.length > 0 && this.state.files.map((file)=>{
-                  return <img style={{height:"180px", width:"180px"}} src={URL.createObjectURL(file)} />
+                !this.state.submit && this.state.files.length > 0 && this.state.files.map((file)=>{
+                    return <PostCard post={{image:URL.createObjectURL(file),label:null,confidence:null}}/> 
                 })
               }
-            </div> */}
-            <Box style = {{background: '#efefef', display: 'flex', transition: 'all 250ms ease-in-out 0s', height: 'calc(80vh - 80px)', width: 'calc(80vw - 80px)', border: 'solid 40px transparent', position: 'relative', alignItems:'center', justifyContent: 'center'}}>
-              {upload}
-            
-              <Button variant="contained" color="primary" onClick={()=>{console.log("Vah");this.handleSubmit()}}>
+            </div>
+            {
+              this.state.posts.length > 0 &&
+                this.state.selected > 0 &&
+                  <div>
+
+                  </div>
+            }
+            <div style={{display:"grid",gridTemplateColumns:"1fr 1fr 1fr 1fr",gridGap:"50px",maxHeight:"60vh",overflowY:"scroll"}}>
+              {
+                this.state.posts.length > 0 && this.state.posts.map((post)=>{
+                    return <PostCard post={post}/> 
+                })
+              }
+            </div>
+          </div>  
+          <Button style={{display:"inline-block"}} variant="contained" color="primary" onClick={()=>{console.log("Vah");this.handleSubmit()}}>
                 Test
-              </Button>
-              
-            </Box>  
-          </DragAndDrop>  
-           
+          </Button>
+
       </div>
       )
     }
